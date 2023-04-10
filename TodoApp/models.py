a870bbd5fd55687db08f84175f52f90f37e34c34
Ramankauldhar <rmnkaul979697@gmail.com>
2023-04-09 21:44:11 -0400

    V3: SQLite DB is creatd and add function is performed

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class TodoList(models.Model):
+    task = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    date = models.CharField(max_length=20)
 
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import TodoList
 
 # Create your views here.
 
@@ -6,5 +7,21 @@ def home(request):
     return render(request, "TodoApp/home.html", {})
 
 def addList(request):
+    if request.method == 'POST':
+        print("New Task Entry Added in The Database")
+
+        #retrieve the user inputs 
+        user_task = request.POST.get("task")
+        user_description = request.POST.get("description")
+        user_date = request.POST.get("dateandtime")
+
+        #create an object for models
+        list = TodoList()
+        list.task = user_task
+        list.description = user_description
+        list.date = user_date
+        list.save()
+        return redirect("/TodoApp/home")
+
     return render(request, "TodoApp/addList.html", {})
 
